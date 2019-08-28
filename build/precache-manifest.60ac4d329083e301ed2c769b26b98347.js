19de8fdc0150951f1de1ced9cbcd00a467c44760
EliTu <eliad91@gmail.com>
2019-08-28 11:59:50 +0800

    Build for deployment

---
+++ build/precache-manifest.60ac4d329083e301ed2c769b26b98347.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf5258829695f0a27a1fbafe4939fe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73fe2cb88c34ca20d5d9",
+    "url": "/static/css/3.8b55c582.chunk.css"
+  },
+  {
+    "revision": "e65b1441f31dfd1c7b89",
+    "url": "/static/css/4.ca15d82a.chunk.css"
+  },
+  {
+    "revision": "ee6434b5a2ee7353262a",
+    "url": "/static/css/5.14390e0f.chunk.css"
+  },
+  {
+    "revision": "01e5b03c36a527cad4e6",
+    "url": "/static/css/6.9097b627.chunk.css"
+  },
+  {
+    "revision": "f0ada77c7270fba3184b",
+    "url": "/static/css/main.982bdb1e.chunk.css"
+  },
+  {
+    "revision": "2f5a5719f1fedfa95a16",
+    "url": "/static/js/2.cae8e5e4.chunk.js"
+  },
+  {
+    "revision": "73fe2cb88c34ca20d5d9",
+    "url": "/static/js/3.e695fc25.chunk.js"
+  },
+  {
+    "revision": "e65b1441f31dfd1c7b89",
+    "url": "/static/js/4.c7867324.chunk.js"
+  },
+  {
+    "revision": "ee6434b5a2ee7353262a",
+    "url": "/static/js/5.26259fb9.chunk.js"
+  },
+  {
+    "revision": "01e5b03c36a527cad4e6",
+    "url": "/static/js/6.60b6c83c.chunk.js"
+  },
+  {
+    "revision": "f0ada77c7270fba3184b",
+    "url": "/static/js/main.b30110cd.chunk.js"
+  },
+  {
+    "revision": "94d39e485cc936df4758",
+    "url": "/static/js/runtime~main.689d4dfd.js"
+  }
+]);
